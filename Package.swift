34114b9334f9e55d93883a48b12bd3aae23a9b86
Luke Matkins <ljmatkins@gmail.com>
2023-06-23 05:53:12 -0400

    adding visionOS platform

--- Package.swift
+++ Package.swift
@@ -4,192 +4,194 @@
 import PackageDescription
 
 let package = Package(
-    name: "MIDIKit",
-    
-    platforms: [
-        .macOS(.v10_12),
-        .iOS(.v10),
-        .tvOS(.v10), // builds, but no MIDI features
-        .watchOS(.v3) // builds, but no MIDI features
-    ],
-    
-    products: [
-        .library(
-            name: "MIDIKit",
-            type: .static,
-            targets: ["MIDIKit"]
-        ),
-        .library(
-            name: "MIDIKitCore",
-            type: .static,
-            targets: ["MIDIKitCore"]
-        ),
-        .library(
-            name: "MIDIKitIO",
-            type: .static,
-            targets: ["MIDIKitIO"]
-        ),
-        .library(
-            name: "MIDIKitControlSurfaces",
-            type: .static,
-            targets: ["MIDIKitControlSurfaces"]
-        ),
-        .library(
-            name: "MIDIKitSMF",
-            type: .static,
-            targets: ["MIDIKitSMF"]
-        ),
-        .library(
-            name: "MIDIKitSync",
-            type: .static,
-            targets: ["MIDIKitSync"]
-        ),
-        .library(
-            name: "MIDIKitUI",
-            type: .static,
-            targets: ["MIDIKitUI"]
-        )
-    ],
-    
-    dependencies: [
-        .package(url: "https://github.com/orchetect/TimecodeKit", from: "1.6.9"),
-        
-        // testing only:
-        .package(url: "https://github.com/orchetect/XCTestUtils", from: "1.0.1")
-    ],
-    
-    targets: [
-        .target(
-            name: "MIDIKit",
-            dependencies: [
-                .target(name: "MIDIKitCore"),
-                .target(name: "MIDIKitIO"),
-                .target(name: "MIDIKitControlSurfaces"),
-                .target(name: "MIDIKitSMF"),
-                .target(name: "MIDIKitSync"),
-                .target(name: "MIDIKitUI")
-            ],
-            swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
-        ),
-        .target(
-            name: "MIDIKitInternals",
-            dependencies: [],
-            swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
-        ),
-        .target(
-            name: "MIDIKitCore",
-            dependencies: [
-                .target(name: "MIDIKitInternals")
-            ],
-            swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
-        ),
-        .target(
-            name: "MIDIKitIO",
-            dependencies: [
-                .target(name: "MIDIKitInternals"),
-                .target(name: "MIDIKitCore")
-            ],
-            swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
-        ),
-        .target(
-            name: "MIDIKitControlSurfaces",
-            dependencies: [
-                .target(name: "MIDIKitCore")
-            ],
-            swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
-        ),
-        .target(
-            name: "MIDIKitSMF",
-            dependencies: [
-                .target(name: "MIDIKitCore"),
-                "TimecodeKit"
-            ],
-            swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
-        ),
-        .target(
-            name: "MIDIKitSync",
-            dependencies: [
-                .target(name: "MIDIKitCore"),
-                "TimecodeKit"
-            ],
-            swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
-        ),
-        .target(
-            name: "MIDIKitUI",
-            dependencies: [
-                .target(name: "MIDIKitIO")
-            ],
-            swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
-        ),
-        
-        // test targets
-        .testTarget(
-            name: "MIDIKitCoreTests",
-            dependencies: [
-                .target(name: "MIDIKitCore"),
-                .product(name: "XCTestUtils", package: "XCTestUtils")
-            ]
-        ),
-        .testTarget(
-            name: "MIDIKitIOTests",
-            dependencies: [
-                .target(name: "MIDIKitIO"),
-                .product(name: "XCTestUtils", package: "XCTestUtils")
-            ]
-        ),
-        .testTarget(
-            name: "MIDIKitControlSurfacesTests",
-            dependencies: [
-                .target(name: "MIDIKitControlSurfaces"),
-                .product(name: "XCTestUtils", package: "XCTestUtils")
-            ]
-        ),
-        .testTarget(
-            name: "MIDIKitSMFTests",
-            dependencies: [
-                .target(name: "MIDIKitSMF"),
-                .product(name: "XCTestUtils", package: "XCTestUtils")
-            ]
-        ),
-        .testTarget(
-            name: "MIDIKitSyncTests",
-            dependencies: [
-                .target(name: "MIDIKitSync"),
-                .product(name: "XCTestUtils", package: "XCTestUtils")
-            ]
-        )
-    ],
-    
-    swiftLanguageVersions: [.v5]
+  name: "MIDIKit",
+
+  platforms: [
+    .macOS(.v10_12),
+    .iOS(.v10),
+    .tvOS(.v10),  // builds, but no MIDI features
+    .watchOS(.v3),  // builds, but no MIDI features
+    .visionOS(.v1),
+  ],
+
+  products: [
+    .library(
+      name: "MIDIKit",
+      type: .static,
+      targets: ["MIDIKit"]
+    ),
+    .library(
+      name: "MIDIKitCore",
+      type: .static,
+      targets: ["MIDIKitCore"]
+    ),
+    .library(
+      name: "MIDIKitIO",
+      type: .static,
+      targets: ["MIDIKitIO"]
+    ),
+    .library(
+      name: "MIDIKitControlSurfaces",
+      type: .static,
+      targets: ["MIDIKitControlSurfaces"]
+    ),
+    .library(
+      name: "MIDIKitSMF",
+      type: .static,
+      targets: ["MIDIKitSMF"]
+    ),
+    .library(
+      name: "MIDIKitSync",
+      type: .static,
+      targets: ["MIDIKitSync"]
+    ),
+    .library(
+      name: "MIDIKitUI",
+      type: .static,
+      targets: ["MIDIKitUI"]
+    ),
+  ],
+
+  dependencies: [
+    .package(url: "https://github.com/orchetect/TimecodeKit", from: "1.6.9"),
+
+    // testing only:
+    .package(url: "https://github.com/orchetect/XCTestUtils", from: "1.0.1"),
+  ],
+
+  targets: [
+    .target(
+      name: "MIDIKit",
+      dependencies: [
+        .target(name: "MIDIKitCore"),
+        .target(name: "MIDIKitIO"),
+        .target(name: "MIDIKitControlSurfaces"),
+        .target(name: "MIDIKitSMF"),
+        .target(name: "MIDIKitSync"),
+        .target(name: "MIDIKitUI"),
+      ],
+      swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
+    ),
+    .target(
+      name: "MIDIKitInternals",
+      dependencies: [],
+      swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
+    ),
+    .target(
+      name: "MIDIKitCore",
+      dependencies: [
+        .target(name: "MIDIKitInternals")
+      ],
+      swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
+    ),
+    .target(
+      name: "MIDIKitIO",
+      dependencies: [
+        .target(name: "MIDIKitInternals"),
+        .target(name: "MIDIKitCore"),
+      ],
+      swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
+    ),
+    .target(
+      name: "MIDIKitControlSurfaces",
+      dependencies: [
+        .target(name: "MIDIKitCore")
+      ],
+      swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
+    ),
+    .target(
+      name: "MIDIKitSMF",
+      dependencies: [
+        .target(name: "MIDIKitCore"),
+        "TimecodeKit",
+      ],
+      swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
+    ),
+    .target(
+      name: "MIDIKitSync",
+      dependencies: [
+        .target(name: "MIDIKitCore"),
+        "TimecodeKit",
+      ],
+      swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
+    ),
+    .target(
+      name: "MIDIKitUI",
+      dependencies: [
+        .target(name: "MIDIKitIO")
+      ],
+      swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
+    ),
+
+    // test targets
+    .testTarget(
+      name: "MIDIKitCoreTests",
+      dependencies: [
+        .target(name: "MIDIKitCore"),
+        .product(name: "XCTestUtils", package: "XCTestUtils"),
+      ]
+    ),
+    .testTarget(
+      name: "MIDIKitIOTests",
+      dependencies: [
+        .target(name: "MIDIKitIO"),
+        .product(name: "XCTestUtils", package: "XCTestUtils"),
+      ]
+    ),
+    .testTarget(
+      name: "MIDIKitControlSurfacesTests",
+      dependencies: [
+        .target(name: "MIDIKitControlSurfaces"),
+        .product(name: "XCTestUtils", package: "XCTestUtils"),
+      ]
+    ),
+    .testTarget(
+      name: "MIDIKitSMFTests",
+      dependencies: [
+        .target(name: "MIDIKitSMF"),
+        .product(name: "XCTestUtils", package: "XCTestUtils"),
+      ]
+    ),
+    .testTarget(
+      name: "MIDIKitSyncTests",
+      dependencies: [
+        .target(name: "MIDIKitSync"),
+        .product(name: "XCTestUtils", package: "XCTestUtils"),
+      ]
+    ),
+  ],
+
+  swiftLanguageVersions: [.v5]
 )
 
 func addShouldTestFlag(toTarget targetName: String) {
-    // swiftSettings may be nil so we can't directly append to it
-    
-    var swiftSettings = package.targets
-        .first(where: { $0.name == targetName })?
-        .swiftSettings ?? []
-    
-    swiftSettings.append(.define("shouldTestCurrentPlatform"))
-    
+  // swiftSettings may be nil so we can't directly append to it
+
+  var swiftSettings =
     package.targets
-        .first(where: { $0.name == targetName })?
-        .swiftSettings = swiftSettings
+    .first(where: { $0.name == targetName })?
+    .swiftSettings ?? []
+
+  swiftSettings.append(.define("shouldTestCurrentPlatform"))
+
+  package.targets
+    .first(where: { $0.name == targetName })?
+    .swiftSettings = swiftSettings
 }
 
 func addShouldTestFlags() {
-    addShouldTestFlag(toTarget: "MIDIKitCoreTests")
-    addShouldTestFlag(toTarget: "MIDIKitIOTests")
-    addShouldTestFlag(toTarget: "MIDIKitControlSurfacesTests")
-    addShouldTestFlag(toTarget: "MIDIKitSMFTests")
-    addShouldTestFlag(toTarget: "MIDIKitSyncTests")
+  addShouldTestFlag(toTarget: "MIDIKitCoreTests")
+  addShouldTestFlag(toTarget: "MIDIKitIOTests")
+  addShouldTestFlag(toTarget: "MIDIKitControlSurfacesTests")
+  addShouldTestFlag(toTarget: "MIDIKitSMFTests")
+  addShouldTestFlag(toTarget: "MIDIKitSyncTests")
 }
 
 // Swift version in Xcode 12.5.1 which introduced watchOS testing
 #if os(watchOS) && swift(>=5.4.2)
-addShouldTestFlags()
+  addShouldTestFlags()
 #elseif os(watchOS)
-// don't add flag
+  // don't add flag
 #else
-addShouldTestFlags()
+  addShouldTestFlags()
 #endif
